80eccc840069612f5e54e42fd765c696d2f28880
Dimitri250 <k.dimitri@alustudent.com>
2023-06-02 14:43:38 +0200

    .env is changed

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
-
-const server = require("./server");
-
+const { sequelize } = require("./database/models");
+const { server } = require("./server/Index");
 const PORT = process.env.PORT || 3300;
-
-server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
+const connectDb = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+server.listen(PORT, () => {
+  console.log(`Server is live at localhost:${PORT}`);
+  connectDb();
+});
