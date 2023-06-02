0f45b95419d10b5740ca1a4adf6b98cefb8a0e2c
Dimitri250 <k.dimitri@alustudent.com>
2023-06-02 15:46:24 +0200

    index

--- database/models/index.js
+++ database/models/index.js
@@ -1,13 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-const envConfigs = require("../config/config");
-
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = envConfigs[env];
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
-
 let sequelize;
 if (config.url) {
   sequelize = new Sequelize(config.url, config);
@@ -19,28 +16,27 @@ if (config.url) {
     config
   );
 }
-
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
   .forEach((file) => {
-   const model = require(path.join(__dirname, file))(
-     sequelize,
-     Sequelize.DataTypes
-   );
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
-
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 module.exports = db;
